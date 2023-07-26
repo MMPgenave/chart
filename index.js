524701f3245c313d6ec0e4bc68e360c71e41cb51
MMPGenave <mohammadmp14@gmail.com>
2023-07-26 15:32:43 +0330

    first

---
+++ index.js
@@ -0,0 +1,79 @@
+const columnHeight = document.getElementById("colA").clientHeight; //384px or in tailwing h-96
+const ballHeight = document.getElementById("ball1").clientHeight; //16px
+
+//in initial page loading we call a function
+window.onload = function () {
+  move_ball_1();
+  move_ball_2();
+  move_ball_3();
+};
+
+//set positions of Line AC
+let inputValue1 = document.getElementById("input1").value;
+let inputValue2 = document.getElementById("input2").value;
+let x1_Line_AC = 215;
+let y1_Line_AC = -(columnHeight / 100) * inputValue1 + columnHeight;
+let x2_Line_AC = 395;
+let y2_Line_AC = -(columnHeight / 100) * inputValue2 + columnHeight;
+
+//set positions of Line CN
+let inputValue3 = document.getElementById("input3").value;
+let x1_Line_CN = 395;
+let y1_Line_CN = -(columnHeight / 100) * inputValue2 + columnHeight;
+let x2_Line_CN = 570;
+let y2_Line_CN = -(columnHeight / 100) * inputValue3 + columnHeight;
+
+function drawLineAC() {
+  let LineAC = document.getElementById("LineAC");
+  LineAC.y1.baseVal.value = y1_Line_AC;
+  LineAC.y2.baseVal.value = y2_Line_AC;
+}
+
+function drawLineCN() {
+  let LineCN = document.getElementById("LineCN");
+  LineCN.y1.baseVal.value = y1_Line_CN;
+  LineCN.y2.baseVal.value = y2_Line_CN;
+}
+
+function move_ball_3() {
+  const ball3 = document.getElementById("ball3");
+  let inputValue3 = document.getElementById("input3").value;
+  if (inputValue3 > 100) {
+    alert(`عدد وارد شده باید از 100 کمتر باشد`);
+    inputValue3 = 40;
+  }
+  // I do a little math! and got the following equation based on stright line  (y = -3.8x + 380)
+  ball3.style.top =
+    -(columnHeight / 100) * inputValue3 + columnHeight - ballHeight / 2 + "px";
+  y2_Line_CN = -(columnHeight / 100) * inputValue3 + columnHeight;
+  drawLineCN();
+}
+function move_ball_2() {
+  const ball2 = document.getElementById("ball2");
+  let inputValue2 = document.getElementById("input2").value;
+  if (inputValue2 > 100) {
+    alert(`عدد وارد شده باید از 100 کمتر باشد`);
+    inputValue2 = 80;
+  }
+  ball2.style.top =
+    -(columnHeight / 100) * inputValue2 + columnHeight - ballHeight / 2 + "px";
+  y2_Line_AC = -(columnHeight / 100) * inputValue2 + columnHeight;
+  y1_Line_CN = y2_Line_AC;
+  drawLineAC();
+  drawLineCN();
+}
+function move_ball_1() {
+  const ball1 = document.getElementById("ball1");
+  let inputValue1 = document.getElementById("input1").value;
+  if (inputValue1 > 100) {
+    alert(`عدد وارد شده باید از 100 کمتر باشد`);
+    inputValue1 = 20;
+  }
+
+  // I do a little math! and got the following equation based on stright line
+  let ball_y_value =
+    -(columnHeight / 100) * inputValue1 + columnHeight - ballHeight / 2;
+  ball1.style.top = ball_y_value + "px"; //8 is half of ball height and 384 is height of column A
+  y1_Line_AC = ball_y_value + ballHeight / 2; // but for the line to go through the center of ball we delete (-8px) value
+  drawLineAC();
+}
